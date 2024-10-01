ff354335fd16f87c6c86a6ea679319b554422d19
dearguru07 <pangalaguruyadav101@gmail.com>
2024-10-01 09:11:05 +0530

    'p'

---
+++ app.js
@@ -0,0 +1,5 @@
+function Click(){
+    let data=fetch("https://d2330pufzy0cvr.cloudfront.net/carousel-products/e214fead-96be-4825-b935-d602198e1914-4EFBA2BDEE2676B85C2878B615B63CE2.json")
+    let OriginalData=data.json()
+    console.log(OriginalData)
+}
